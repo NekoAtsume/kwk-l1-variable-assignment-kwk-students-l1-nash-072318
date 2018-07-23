31fb06648c9acd99dd63c60b9b7e108fa26c993f
NekoAtsume <wolvesofgrace@yahoo.com>
2018-07-23 19:26:04 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -5,7 +5,7 @@
 #name = temmi
 #pizza = tem 
 #puts " Hoi!! Moi name s #{name} and my favs fod s #{pizza}"
-favword = supercalifragilisticexpialidocious
+favword = "supercalifragilisticexpialidocious"
 name = "Della"
 intrest = "Art"
 school = "GMS"
