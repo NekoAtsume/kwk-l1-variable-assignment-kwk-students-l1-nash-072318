4d732e9cf43cc6493c266675ab6c48907f12d112
NekoAtsume <wolvesofgrace@yahoo.com>
2018-07-23 19:16:10 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,7 +1,7 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
-"greeting" = 42
-"Hello World" =  42
+"greeting" 
+"Hello World"
 name = Temmi
 pizza = tem 
 puts " Hoi!! Moi name s #{name} and my favs fod s #{pizza}"
